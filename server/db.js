0a3b9dcda907e3cbac1eb92a72f0effbc6fdf037
Dankirigiri <dankirigiri5@gmail.com>
2022-11-25 15:45:38 -0300

    Database error fixed

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
 import { createConnection } from 'mysql2/promise'
 
-export const pool = createConnection(DATABASE_URL);
+export const pool = createConnection('mysql://ahbn41353aqyv1ly746k:pscale_pw_iwa5Uj6xH5dM5KqeMY7WwdkOc5sRlbmAzviuoIGIz@us-east.connect.psdb.cloud/prueba?ssl={"rejectUnauthorized":true}');
 console.log("DB init")
